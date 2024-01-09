bcccaf321298d7d2b80e15fe04f045ffff943203
anand <rd.anandv@gmail.com>
2024-01-09 21:53:32 +0530

    angular crud app

---
+++ 24.crud_app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '24.crud_app';
+
+  getAlert(){
+    alert("Hello World!")
+  }
+}
