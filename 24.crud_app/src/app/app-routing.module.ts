38a461205aeda30dee022dd8d108d34741e9f11e
anand <rd.anandv@gmail.com>
2024-01-14 11:28:48 +0530

    crud completed

--- 24.crud_app/src/app/app-routing.module.ts
+++ 24.crud_app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
 
 const routes: Routes = [
-  {path:"",component:HomePageComponent}
+  { path: "", component: HomePageComponent },
+  { path: "edit-user/:id", component: EditUserComponent }
 ];
 
 @NgModule({
--- 24.crud_app/src/app/app.module.ts
+++ 24.crud_app/src/app/app.module.ts
@@ -4,16 +4,21 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    EditUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
