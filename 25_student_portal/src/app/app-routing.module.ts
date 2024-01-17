3016da83d1605f370f59ca9dae7ebe4ea1397ef2
anand <rd.anandv@gmail.com>
2024-01-17 21:19:48 +0530

    dashboard page added and form validation working

--- 25_student_portal/src/app/app-routing.module.ts
+++ 25_student_portal/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { RegisterComponent } from './user/register/register.component';
 import { LoginComponent } from './user/login/login.component';
 import { ContactUsComponent } from './user/contact-us/contact-us.component';
 import { DashboardComponent } from './user/dashboard/dashboard.component';
+import { ProfileComponent } from './user/profile/profile.component';
 
 const routes: Routes = [
   { path: "", component: HomepageComponent },
   { path: "register", component: RegisterComponent },
   { path: "login", component: LoginComponent },
   { path: "contact", component: ContactUsComponent },
-  { path: "dashboard", component: DashboardComponent }
+  { path: "dashboard", component: DashboardComponent },
+  { path: "profile", component: ProfileComponent }
 ];
 
 @NgModule({
--- 25_student_portal/src/app/header/header.component.ts
+++ 25_student_portal/src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  selectedOption: string = "Login";
   constructor() { }
 
   ngOnInit(): void {
