4caeca2e07086dec3b6ef350b1f37279c19833c4
anand <rd.anandv@gmail.com>
2024-01-18 21:59:41 +0530

    validation added in all form & model service created

--- 25_student_portal/src/app/user/contact-us/contact-us.component.ts
+++ 25_student_portal/src/app/user/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
-
+  contactForm!: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.contactForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      comment: new FormControl('', [Validators.required, Validators.minLength(10)])
+    })
+
+  }
+
+  get name() {
+    return this.contactForm.get("name");
+  }
+  get email() {
+    return this.contactForm.get('email');
+  }
+  get comment() {
+    return this.contactForm.get("comment");
   }
 
 }
